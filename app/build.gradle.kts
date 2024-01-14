c0d367857b95b1f278b5aa86b983fad2980a1e10
Magdy <momagdy010299@gmail.com>
2024-01-14 12:17:30 +0200

    first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,121 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.instagram_app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.instagram_app"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("com.google.firebase:firebase-firestore:24.10.0")
+    implementation("com.google.firebase:firebase-storage:20.3.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    implementation ("androidx.navigation:navigation-compose:2.7.6")
+
+    // Dagger - Hilt
+    implementation("com.google.dagger:hilt-android:2.48")
+    kapt ("com.google.dagger:hilt-android-compiler:2.48")
+//    kapt ("androidx.hilt:hilt-compiler:1.1.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
+
+    // Coroutines
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+//    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
+//    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+//    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+//    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+//
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.3.0")
+
+    //firebase
+//    implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
+//    implementation("com.google.firebase:firebase-firestore-ktx:24.10.0")
+//    implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
+
+    implementation ("com.google.firebase:firebase-auth:22.3.0")
+    implementation (platform("com.google.firebase:firebase-bom:29.0.0"))
+    implementation ("com.google.firebase:firebase-auth-ktx")
+    implementation ("com.google.firebase:firebase-firestore-ktx")
+    implementation ("com.google.firebase:firebase-storage-ktx")
+
+    // Retrofit
+//    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+//
+    // OkHttp
+//    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+//
+    // JSON Converter
+//    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+//
+    //preferences
+//    implementation("androidx.datastore:datastore-preferences:1.1.0-beta01")
+
+    //swipe refresh
+    //  implementation("com.google.accompanist:accompanist-swiperefresh:0.25.1")
+
+    //implementation("androidx.appcompat:appcompat:1.6.1")
+}
+kapt {
+    correctErrorTypes = true
+}
---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.2.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.google.dagger.hilt.android") version "2.45" apply false
+    id("com.google.gms.google-services") version "4.3.15" apply false
+}
